438c4ecbc6f5bbdbe2bb52a9816e8988d615482c
LuiieClone <dbsldkfls@gmail.com>
2024-09-23 16:27:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/my1stapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.my1stapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val clickButton = findViewById<Button>(R.id.clickButton)
+        var clicked = true
+        val textview = findViewById<TextView>(R.id.textView2)
+
+        clickButton.setOnClickListener{
+            if (clicked){
+                textview.text = "CLICKED!!!"
+            } else{
+                textview.text = "Click!"
+            }
+            clicked = !clicked
+        }
+    }
+}
