97e1e2dfecc400da700da5eb5aa58e480646ed23
LuiieClone <dbsldkfls@gmail.com>
2024-09-28 15:02:43 +0900

    2nd app, Data saved

--- app/src/main/java/com/example/my1stapplication/MainActivity.kt
+++ app/src/main/java/com/example/my1stapplication/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.my1stapplication
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.preference.PreferenceManager
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +15,24 @@ import com.example.my1stapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val binding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private fun saveData(height: Float, weight: Float){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putFloat("KEY_HEIGHT", height).putFloat("KEY_WEIGHT", weight).apply()
+    }
+
+    private fun loadData(){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height = pref.getFloat("KEY_HEIGHT", 0f)
+        val weight = pref.getFloat("KEY_WEIGHT", 0f)
+
+        if(height != 0f && weight != 0f){
+            binding.heightEditText.setText(height.toString())
+            binding.weightEditText.setText(weight.toString())
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,13 +58,22 @@ class MainActivity : AppCompatActivity() {
             clicked = !clicked
         }
 
+        loadData()
+
         binding.resultButton2.setOnClickListener {
             if(binding.weightEditText.text.isNotBlank() && binding.heightEditText.text.isNotBlank()){
+                saveData(
+                    binding.heightEditText.text.toString().toFloat(),
+                    binding.weightEditText.text.toString().toFloat(),
+                )
+
                 val intent = Intent(this, ResultActivity::class.java).apply{
                     putExtra("height", binding.heightEditText.text.toString().toFloat())
                     putExtra("weight", binding.weightEditText.text.toString().toFloat())
                 }
                 startActivity(intent)
+            } else{
+                Toast.makeText(this, "Enter the Height/Weight", Toast.LENGTH_SHORT).show()
             }
         }
     }
